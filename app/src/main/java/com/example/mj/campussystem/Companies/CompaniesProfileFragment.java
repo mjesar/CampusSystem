9b7ca2b3cabe8a967f235c67411456f4be4098b1
mjesar <mohammadalijaisar@gmail.com>
2018-03-08 21:47:47 +0500

    Commit message

---
+++ app/src/main/java/com/example/mj/campussystem/Companies/CompaniesProfileFragment.java
@@ -0,0 +1,26 @@
+package com.example.mj.campussystem.Companies;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.mj.campussystem.R;
+
+public class CompaniesProfileFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.activity_companies_profile_fragment,container,false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getActivity().setTitle("Profile");
+    }
+}
